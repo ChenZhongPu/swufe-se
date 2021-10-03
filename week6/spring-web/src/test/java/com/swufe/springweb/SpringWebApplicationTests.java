fb8c682c5f2b4c46dcebc979fb8b4ab46ad33818
chenzhongpu <chenloveit@gmail.com>
2021-10-03 22:37:33 +0800

    add a simple spring web for travis

---
+++ week6/spring-web/src/test/java/com/swufe/springweb/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swufe.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
