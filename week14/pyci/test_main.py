bfafecfbd99e374924736c649632199fa0d5e42a
CHEN zhongpu <chenloveit@gmail.com>
2022-05-15 19:21:20 +0800

    github action: python

---
+++ week14/pyci/test_main.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+import unittest
+import numpy as np
+from main import compute_eig
+
+
+class Test(TestCase):
+    def test_compute_eig(self):
+        A = np.array([[3, -1], [-1, 3]])
+        self.assertAlmostEqual(compute_eig(A), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
