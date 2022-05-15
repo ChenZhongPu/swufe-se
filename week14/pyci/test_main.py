693730359f9b5ee2f7e325dc105313b226a5c065
CHEN zhongpu <chenloveit@gmail.com>
2022-05-15 19:34:29 +0800

    fix pyci

--- week14/pyci/test_main.py
+++ week14/pyci/test_main.py
@@ -7,7 +7,7 @@ from main import compute_eig
 class Test(TestCase):
     def test_compute_eig(self):
         A = np.array([[3, -1], [-1, 3]])
-        self.assertAlmostEqual(compute_eig(A), 3)
+        self.assertAlmostEqual(compute_eig(A), 2)
 
 
 if __name__ == '__main__':
